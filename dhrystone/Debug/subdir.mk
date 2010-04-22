4a46b45ab8498732905daabd4a52f9a3e21fa6ca
jlance <jlance@92547b11-a484-40c4-9575-ed0f39d6bbd3>
2010-04-22 17:44:44 +0000

    Modify using CROSS_COMPILE instead of CTOOL_PREFIX for the IDE

--- dhrystone/Debug/subdir.mk
+++ dhrystone/Debug/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM Sourcery Linux GCC C Compiler'
-	$(CSTOOL_PREFIX)gcc -O0 -march=armv7-a -c -DTIME -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -mcpu=cortex-a8 -g3 -gdwarf-2 -o"$@" "$<"
+	$(CROSS_COMPILE)gcc -O0 -march=armv7-a -c -DTIME -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -mcpu=cortex-a8 -g3 -gdwarf-2 -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
