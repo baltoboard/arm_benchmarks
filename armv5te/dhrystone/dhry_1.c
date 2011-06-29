bf06fa866758b045eb552f4f8bd792b71f93fefc
jlance <jlance@92547b11-a484-40c4-9575-ed0f39d6bbd3>
2011-06-29 16:23:05 +0000

    Adding Legal Headers
    * removing dhrystone_static check

--- armv5te/dhrystone/dhry_1.c
+++ armv5te/dhrystone/dhry_1.c
@@ -1,4 +1,42 @@
 /*
+ * {module name} DHRYSTONE
+ *
+ * {module description}
+ *
+ * Copyright (C) {YEAR} Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+*/
+
+/*
  ****************************************************************************
  *
  *                   "DHRYSTONE" Benchmark Program
@@ -28,7 +66,9 @@ char            Ch_1_Glob,
 int             Arr_1_Glob [50];
 int             Arr_2_Glob [50] [50];
 
-extern char     *malloc ();
+extern char     *malloc (size_t size);
+extern char     *strcpy (char *destination, const char *source);
+
 Enumeration     Func_1 ();
   /* forward declaration necessary since Enumeration may not simply be int */
 
@@ -67,10 +107,12 @@ long            Begin_Time,
 float           Microseconds,
                 Dhrystones_Per_Second;
 
+int             i;
+int             CpuFreq=0; // in KHz
 /* end of variables for time measurement */
 
 
-main ()
+main (int argc, char **argv)
 /*****/
 
   /* main program, corresponds to procedures        */
@@ -118,12 +160,25 @@ main ()
     printf ("Program compiled without 'register' attribute\n");
     printf ("\n");
   }
-  printf ("Please give the number of runs through the benchmark: ");
+  if(argc == 1)
   {
-    int n;
-    scanf ("%d", &n);
-    Number_Of_Runs = n;
+    printf ("Please give the number of runs through the benchmark: ");
+    {
+      int n;
+      scanf ("%d", &n);
+      Number_Of_Runs = n;
+    }
   }
+  else
+  {
+    Number_Of_Runs = atoi(argv[1]);
+    printf("number of runs = %d",Number_Of_Runs);
+  }
+  if(argc == 3)
+  {
+    CpuFreq = atoi(argv[2])/1000;
+  }
+
   printf ("\n");
 
   printf ("Execution starts, %d runs through Dhrystone\n", Number_Of_Runs);
@@ -281,6 +336,11 @@ main ()
     printf ("Dhrystones per Second:                      ");
     printf ("%6.1f \n", Dhrystones_Per_Second);
     printf ("\n");
+    if(CpuFreq !=0)
+    {
+      printf ("CPU clock = %d MHz\n", CpuFreq);
+      printf ("Dhrystone DMIPS/MHz = %6.1f\n", (float)Dhrystones_Per_Second / (float)1757 / (float) CpuFreq);
+    }
   }
   
 }
